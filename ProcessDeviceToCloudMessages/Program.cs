276f81c3fe9627c621a1ef8634b6fbc69145413c
Klavs Hoejberg-Pedersen <klavs.hoejberg-pedersen@hotmail.om>
2017-03-08 11:16:13 +0100

    Project added

---
+++ ProcessDeviceToCloudMessages/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ProcessDeviceToCloudMessages
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var iotHubConnectionString = Config.Config.IoTHubConnectionString;
+            var iotHubD2cEndpoint = Config.Config.IotHubD2CEndpoint;
+            StoreEventProcessor.StorageConnectionString = Config.Config.StorageConnectionString;
+            StoreEventProcessor.ServiceBusConnectionString = Config.Config.ServiceBusConnectionString;
+            var eventProcessorHostName = Guid.NewGuid().ToString();
+            var leaseContainerName = "messages-events";
+
+            var eventProcessorHost = new EventProcessorHost(eventProcessorHostName, iotHubD2cEndpoint, EventHubConsumerGroup.DefaultGroupName, iotHubConnectionString, StoreEventProcessor.StorageConnectionString, leaseContainerName);
+
+            Console.WriteLine("Registering EventProcessor...");
+            eventProcessorHost.RegisterEventProcessorAsync<StoreEventProcessor>().Wait();
+
+            Console.WriteLine("Receiving. Press enter key to stop worker.");
+            Console.ReadLine();
+
+            eventProcessorHost.UnregisterEventProcessorAsync().Wait();
+        }
+    }
+}
--- SimulatedDevice/Program.cs
+++ SimulatedDevice/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Globalization;
 using System.Text;
 using System.Threading.Tasks;
 using Config;
@@ -12,98 +11,48 @@ namespace SimulatedDevice
     internal class Program
     {
         static DeviceClient _deviceClient;
+        static readonly Random Rand = new Random();
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("Simulated device\n");
+            Console.WriteLine("Simulated RFID device\n");
             ServiceBusEnvironment.SystemConnectivity.Mode = ConnectivityMode.Http;
             _deviceClient = DeviceClient.Create(Config.Config.IotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey(Config.Config.DeviceId, Config.Config.DeviceKey));
-
+            
             SendDeviceToCloudMessagesAsync();
+
             Console.ReadLine();
         }
 
         private static async void SendDeviceToCloudMessagesAsync()
         {
-            double avgWindSpeed = 10; // m/s
-            Random rand = new Random();
-
             while (true)
             {
-                var currentWindSpeed = avgWindSpeed + rand.NextDouble() * 4 - 2;
-
                 var telemetryDataPoint = new RfidData
                 {
                     DeviceId = Config.Config.DeviceId,
                     Datetime = DateTime.Now,
-                    RfidTag = currentWindSpeed.ToString(CultureInfo.InvariantCulture)
+                    RfidTag = GetRfidTag()
                 };
-                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
 
-                string levelValue;
-
-                if (rand.NextDouble() > 0.7)
-                {
-                    messageString = "This is a critical message";
-                    levelValue = "critical";
-                }
-                else
-                {
-                    levelValue = "normal";
-                }
-
-                var message = new Message(Encoding.ASCII.GetBytes(messageString));
-                message.Properties.Add("level", levelValue);
-
-                await _deviceClient.SendEventAsync(message);
-                Console.WriteLine("{0} > Sent message: {1}", DateTime.Now, messageString);
-
-                await Task.Delay(1000);
-            }
-        }
-
-        private static async void SendDeviceToCloudInteractiveMessagesAsync()
-        {
-            while (true)
-            {
-                var interactiveMessageString = "Alert message!";
-                var interactiveMessage = new Message(Encoding.ASCII.GetBytes(interactiveMessageString));
-                interactiveMessage.Properties["messageType"] = "interactive";
-                interactiveMessage.MessageId = Guid.NewGuid().ToString();
-
-                await _deviceClient.SendEventAsync(interactiveMessage);
-                Console.WriteLine("{0} > Sending interactive message: {1}", DateTime.Now, interactiveMessageString);
-
-                Task.Delay(10000).Wait();
-            }
-        }
-
-
-
-        private static async void SendDeviceToCloudMessagesAsync1()
-        {
-            double avgWindSpeed = 10; // m/s
-            double avgTemperature = 20;
-            var rand = new Random();
-
-            while (true)
-            {
-                var currentWindSpeed = Math.Round(avgWindSpeed + rand.NextDouble() * 4 - 2, 2);
-                var currentTemperature = Math.Round(avgTemperature + rand.NextDouble(), 1);//* 4 - 2;
-
-                var telemetryDataPoint = new
-                {
-                    deviceId = Config.Config.DeviceId,
-                    windSpeed = currentWindSpeed,
-                    temperature = currentTemperature
-                };
                 var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
                 var message = new Message(Encoding.ASCII.GetBytes(messageString));
-                await _deviceClient.SendEventAsync(message);
-                Console.WriteLine($"{DateTime.Now} > Sending message: {messageString}");
 
-                Task.Delay(1000).Wait();
+                await _deviceClient.SendEventAsync(message);
+                Console.WriteLine($"Sent message: {messageString}");
+
+                await Task.Delay(500);
             }
         }
+        private static string GetRfidTag()
+        {
+            var sb = new StringBuilder(26);
+            for (var i = 0; i < 25; i++)
+            {
+                sb.Append(Rand.Next(0, 9));
+            }
+            return sb.ToString();
+        }
+
     }
 }
