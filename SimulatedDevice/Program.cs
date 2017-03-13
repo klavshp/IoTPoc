d4903f1142f246b505ad3f74095da8185fb40b08
klavshp <klavs.hoejberg-pedersen@hotmail.om>
2017-03-13 09:08:36 +0100

    Now simulating several devices

--- SimulatedDevice/Program.cs
+++ SimulatedDevice/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
 using Config;
+using Microsoft.Azure.Amqp.Serialization;
 using Microsoft.Azure.Devices.Client;
 using Microsoft.ServiceBus;
 using Newtonsoft.Json;
@@ -12,10 +15,32 @@ namespace SimulatedDevice
     {
         private static DeviceClient _deviceClient;
         private static readonly Random Rand = new Random();
+        private static List<Device> _deviceList;
+
+        private class Device
+        {
+            public string DeviceId { get; set; }
+            public string DeviceName { get; set; }
+        } 
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("Starting the simulated RFID device...");
+            _deviceList = new List<Device>()
+            {
+                new Device { DeviceId = "e8dca9e8-b665-4e26-8c1a-43ee72e46e07", DeviceName = "Simulated Device 0" },
+                new Device { DeviceId = "344ad8ca-9464-4548-9307-bf70bc910d3d", DeviceName = "Simulated Device 1" },
+                new Device { DeviceId = "7a813b7e-4d90-499b-9be9-58ac838c8016", DeviceName = "Simulated Device 2" },
+                new Device { DeviceId = "b5e7f7e5-fe12-4b79-9643-717f935c611c", DeviceName = "Simulated Device 3" },
+                new Device { DeviceId = "e5214eb1-6bc9-423a-a803-5556a6975ddd", DeviceName = "Simulated Device 4" },
+                new Device { DeviceId = "9c4b275e-887b-441f-a917-9b2022527b5c", DeviceName = "Simulated Device 5" },
+                new Device { DeviceId = "358c03f9-b143-45ac-9a56-d6026abe8665", DeviceName = "Simulated Device 6" },
+                new Device { DeviceId = "d264dfd2-b89e-4d4f-819c-33c3c4a0b028", DeviceName = "Simulated Device 7" },
+                new Device { DeviceId = "8257de42-6526-4108-9a5c-79902bd43b45", DeviceName = "Simulated Device 8" },
+                new Device { DeviceId = "fee15741-975d-46a8-8f6b-55d8fd443c77", DeviceName = "Simulated Device 9" }
+            };
+
+            Console.WriteLine("Connecting to IoT Hub, please stand by...");
+
             ServiceBusEnvironment.SystemConnectivity.Mode = ConnectivityMode.Http;
             _deviceClient = DeviceClient.Create(
                 Config.Config.IotHubUri, 
@@ -29,12 +54,17 @@ namespace SimulatedDevice
 
         private static async void SendDeviceToCloudMessagesAsync()
         {
+            Console.WriteLine("Starting the RFID device simulation...");
+
             while (true)
             {
+                var random = new Random();
+                var chosenDevice = _deviceList[random.Next(0, 9)];
+
                 var rfidData = new RfidData
                 {
-                    DeviceId = Config.Config.DeviceId,
-                    DeviceName = Config.Config.DeviceName,
+                    DeviceId = chosenDevice.DeviceId,
+                    DeviceName = chosenDevice.DeviceName,
                     Datetime = DateTime.Now,
                     RfidTag = GetRfidTag()
                 };
@@ -46,7 +76,7 @@ namespace SimulatedDevice
 
                 Console.WriteLine($"Sent message: {messageString}");
 
-                await Task.Delay(500);
+                await Task.Delay(100);
             }
         }
         private static string GetRfidTag()
